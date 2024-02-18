c821e5e25bf0270098cda79320ef490da0891a6d
lsj0202 <w1004123@gmail.com>
2024-02-18 23:44:32 +0900

    feat: add toss/useOverlay alertModal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,20 @@
-"use clinet";
+"use client";
+
+import React, { useCallback } from "react";
+import { useOverlay } from "@toss/use-overlay";
 import { Button } from "@/components/shared/Button";
-import Input from "@/components/shared/Input";
-import { Text } from "@/components/shared/Text";
-import TextField from "@/components/shared/TextField";
+import Alert from "@/components/shared/Alert";
 
 const Home = () => {
-  return (
-    <>
-      <Text typography="t1" color="red">
-        t1
-      </Text>
-      <Button size="medium" color="success">
-        클릭
-      </Button>
-      <Button size="large" color="primary">
-        클릭
-      </Button>
-      <Button weak={true} size="small" color="error">
-        클릭
-      </Button>
-      <Button weak={true} full={true} size="small" color="error">
-        클릭
-      </Button>
-      <Button
-        weak={true}
-        disabled={true}
-        full={true}
-        size="small"
-        color="error"
-      >
-        클릭
-      </Button>
-      <Input placeholder="로그인" aria-invalid={true} />
-      <TextField label="id" />
-      <TextField label="password" hasError={true} helpMessage={"하ㅣㅇ"} />
-    </>
-  );
+  const overlay = useOverlay();
+
+  const AlertModal = useCallback(() => {
+    overlay.open(({ isOpen, close }) => (
+      <Alert isOpen={isOpen} onClose={close} />
+    ));
+  }, [overlay]);
+
+  return <Button onClick={AlertModal}>Overlay Open</Button>;
 };
 
 export default Home;
--- src/components/shared/Alert.tsx
+++ src/components/shared/Alert.tsx
@@ -1,15 +1,67 @@
+"use client";
+
 import React from "react";
+import Dimmed from "./Dimmed";
+import { styled } from "styled-components";
+import { colors } from "@/styles/colorPalette";
+import { Text } from "./Text";
+import Flex from "./Flex";
+import { Button } from "./Button";
 
 type AlertProps = {
-  open?: boolean;
+  isOpen?: boolean;
   title?: React.ReactNode;
   description?: React.ReactNode;
   buttonLabel?: string;
-  onButtonClick?: () => void;
+  onClose?: () => void;
 };
 
-const Alert = ({ open }: AlertProps) => {
-  return <div></div>;
+const Alert = ({
+  isOpen,
+  title,
+  description,
+  buttonLabel = "확인",
+  onClose,
+}: AlertProps) => {
+  if (isOpen == false) return null;
+  return (
+    <Dimmed>
+      <AlertContainer>
+        <Text
+          typography="t4"
+          bold={true}
+          display="block"
+          style={{ marginBottom: 6 }}
+        >
+          {title}
+        </Text>
+        {description && <Text typography="t7">{description}</Text>}
+        <Flex justify="flex-end">
+          <Button
+            onClick={onClose}
+            weak={true}
+            style={{ marginTop: 12, border: "none" }}
+          >
+            {buttonLabel}
+          </Button>
+        </Flex>
+      </AlertContainer>
+    </Dimmed>
+  );
 };
 
 export default Alert;
+
+const AlertContainer = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  background-color: ${colors.white};
+  border-radius: 8px;
+  overflow: hidden;
+  z-index: var(--alert-zindex);
+  width: 320px;
+  padding: 24px;
+  box-sizing: border-box;
+`;
